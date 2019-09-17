106eb3f7593eb4c3684cfabc1ce75c11260cbb2e
Vishnevskaya Nastya <vishnevskayanastyao@gmail.com>
2019-09-17 12:23:50 +0700

    Initial commit.

--- case1.py
+++ case1.py
@@ -1,1 +1,11 @@
-#hgff
+# Case-study #1
+# Developers:  Revtova L. (20%)
+#              Nikitina A. (60%)
+#              Arhipova A (30%)
+import turtle
+def triangle_1(x, y, color, size)
+    turtle.up()
+    turtle.setpositions(x,y)
+    turtle.down()
+    turtle.forward(50)
+    turtle.forward
